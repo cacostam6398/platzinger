e0578a5b9ee4ee68535931cc5dbca8b034d2ea46
David Acosta <36524775+cacostam6398@users.noreply.github.com>
2019-06-13 17:04:33 -0500

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
+import { UserService } from '../services/user.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user:User;
+  constructor(private userservice:UserService, private authenticationService:AuthenticationService) { 
+    this.authenticationService.getStatus().subscribe((next)=> {
+      this.userservice.getUserById(next.uid).valueChanges().subscribe((data : User) =>
+      {
+          this.user = data;
+          console.log(this.user);
+      },(error)=>{
+        console.log(error)
+      });
+    },(error) =>{
+      console.log(error)
+    });
+  }
 
   ngOnInit() {
   }
